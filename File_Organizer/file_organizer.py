9cdd6285d67c7a22430d38873db3f5df8d29f621
Adrian <it.adrianh@gmail.com>
2025-04-03 03:43:44 +0700

    Add files via upload

---
+++ File_Organizer/file_organizer.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+
+source_folder = "C:/Users/Xbravo/Downloads"
+extension_map = {
+    "Images": [".jpg", ".png", ".gif"],
+    "Documents": [".pdf", ".docx", ".xlsx"],
+    "Software": [".exe", ".msi"],
+    "Archives": [".zip", ".rar", ".7z"]
+}
+
+for folder in extension_map.keys():
+    os.makedirs(os.path.join(source_folder, folder), exist_ok=True)
+
+for file in os.listdir(source_folder):
+    file_path = os.path.join(source_folder, file)
+    if os.path.isfile(file_path):
+        for folder, extensions in extension_map.items():
+            if any(file.lower().endswith(ext) for ext in extensions):
+                shutil.move(file_path, os.path.join(source_folder, folder, file))
+                break
